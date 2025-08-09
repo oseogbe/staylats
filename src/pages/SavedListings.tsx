42afc5e0f740cb0b0e6fb08e238259459f0239a7
Osememen Ogbe <osememen.ogbe@gmail.com>
2025-08-09 17:36:33 +0100

    ui: saved listings update

--- src/pages/SavedListings.tsx
+++ src/pages/SavedListings.tsx
@@ -54,7 +54,7 @@ const SavedListings = () => {
               </p>
             </div>
             
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
               {savedProperties.map((property) => (
                 <PropertyCard key={property.id} property={property} />
               ))}
